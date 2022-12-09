c9e4f3833320c7ae1883fe9f116cf9a766dc8fcc
nova-1001 <nova4710@outlook.jp>
2022-12-09 23:15:04 +0900

    Page3作成完了

--- src/views/page3View.vue
+++ src/views/page3View.vue
@@ -1,7 +1,17 @@
 <template>
   <title>Question2</title>
-  <h1>緑色を表示せよ</h1>
-  <h2>ヒント：回答欄にスペルを入力すると色が変化するぞ</h2>
+  <h1>パスワードを探せ</h1>
+  <h2>ヒント：緑色を表示せよ</h2>
+  <section>
+    <input
+      type="number"
+      placeholder="色の名前を入れると?"
+      min="0"
+      max="255"
+      v-model="green"
+    />
+    <input type="range" min="0" max="1" step="0.1" v-model="alpha" />
+  </section>
   <section id="page3">
     <label>解答欄<input type="text" size="40" /></label>
     <button v-on:click="next">回答</button>
@@ -11,13 +21,21 @@
     </div>
   </section>
   <div class="app">
-    <p>rgba( {{ red }}, {{ green }}, 200, 0.5 )</p>
+    <p>rgba( {{ red }}, {{ green }}, {{ blue }}, {{ alpha }})</p>
     <div
       class="palette"
       v-on:mousemove="changeColor"
       v-on:click="pickColor"
       v-bind:style="{ backgroundColor: `rgba(${red}, ${green}, 200, 0.5)` }"
-    ></div>
+    >
+      <p
+        v-if="passDisplay"
+        v-on:mousemove="changeAlpha"
+        v-bind:style="{ color: `rgba(200, 150, 0, ${alphaNum})` }"
+      >
+        mousemove
+      </p>
+    </div>
 
     <div class="colors-container">
       <div class="mini-palette"></div>
@@ -31,11 +49,23 @@ export default {
     return {
       red: 0,
       green: 0,
+      blue: 0,
+      alpha: 0.5,
       colors: [
         // { red: 0, green: 0 }
       ],
+      alphaNum: 0,
       answer: false,
     }
+  },
+  computed: {
+    passDisplay() {
+      if (this.green === 150) {
+        return true
+      } else {
+        return false
+      }
+    },
   },
   methods: {
     next: function () {
@@ -45,7 +75,11 @@ export default {
 
     changeColor(e) {
       this.red = e.offsetX
-      this.green = e.offsetY
+      this.blue = e.offsetY
+      this.alphaNum = e.offsetY * 0.00392
+    },
+    changeAlpha(f) {
+      this.alphaNum = f.offsetY
     },
     pickColor() {
       const newColor = {
@@ -60,13 +94,6 @@ export default {
     },
     Page3commentaryView() {
       this.$router.push("/")
-    },
-  },
-  computed: {
-    paletteStyle() {
-      return {
-        backgroundColor: `rgba(${this.red}, ${this.green}, 200, 0.5)`,
-      }
     },
   },
 }
