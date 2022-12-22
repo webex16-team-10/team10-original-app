307385ee2ce141f004ffb070db0faf92f1a92d16
nova-1001 <nova4710@outlook.jp>
2022-12-22 21:30:46 +0900

    Page5css作成完了

--- src/views/page3commentaryView.vue
+++ src/views/page3commentaryView.vue
@@ -1,8 +1,10 @@
 <template>
-  <title>ページ3 解説</title>
-  <h1>mousemoveについて</h1>
-  <h2>ページ上でマウスを動かすと、 mousemove イベントが行われます。</h2>
-  <button v-on:click="page4">次へ</button>
+  <div class="allcontainer">
+    <title>ページ3 解説</title>
+    <h1>mousemoveについて</h1>
+    <h2>ページ上でマウスを動かすと、 mousemove イベントが行われます。</h2>
+    <button v-on:click="page4">次へ</button>
+  </div>
 </template>
 <script>
 export default {
